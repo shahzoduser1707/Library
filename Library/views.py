f6e8cfb3762481bead8a3ae5061f289014e1b140
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-09-27 20:26:40 +0500

    abdulaziz

--- Library/views.py
+++ Library/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+# Category
+class CategoryApiView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAuthenticated, )
+
+class CategoryCreateView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
+
+class CategoryUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
+
+
+# Author
+class AuthorApiView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated,)
+
+class AuthorCreateApi(generics.CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
+
+class AuthorUpdateApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
+
+
+# Genre
+
+class GenreApiView(generics.ListAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAuthenticated,)
+
+class GenreCreateApi(generics.CreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
+
+class GenreUpdateApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
+
+# Book
+class BookApiView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated,)
+
+class BookCreateApi(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
+
+class BookUpdateApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated, 'Name_Permission_Class')
