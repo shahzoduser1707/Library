433429387b119d1b63528f052ee1822a4eb1c3ca
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-09-28 14:52:26 +0500

    translation

---
+++ Library/translation.py
@@ -0,0 +1,22 @@
+from modeltranslation.translator import translator, TranslationOptions
+from .models import Category, Book, Author, Genre
+
+
+class CategoryTranslation(TranslationOptions):
+    fields = ('category_name')
+
+class BookTranslation(TranslationOptions):
+    fields = ('book_bio', 'category', 'genre')
+
+class AuthorTranslation(TranslationOptions):
+    fields = ('bio', 'category', 'genre')
+
+class GenreTranslation(TranslationOptions):
+    fields = ('genre_name')
+
+
+translator.register(Category, CategoryTranslation)
+translator.register(Book, BookTranslation)
+translator.register(Author, AuthorTranslation)
+translator.register(Genre, GenreTranslation)
+
